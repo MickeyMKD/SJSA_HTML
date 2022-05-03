e06d7b2eb7e6065133b7fc3c657ba03c236e1e92
Milan Lichev <milan.lichev@gmail.com>
2022-05-03 22:04:13 +0200

    Added JavaScript folder

---
+++ SJSA_G18/JavaScript/Classes/c1/test.js
@@ -0,0 +1,49 @@
+// console.log("Hello!")
+// console.log("Semos")
+// console.log("Academy")
+
+// deklaracija / definicija na promenliva
+var greeting = "Hello Semos Academy"
+// moze da se koristat i edinechni navodnici ' ' 
+
+// vrednosta na konstantata ne moze da se smeni
+const godina = 2022
+
+// deklaracija na promenliva
+let imeIPrezime
+// zadavanje na vrednost na promenliva
+imeIPrezime = "Milan"
+
+// koristenje na promenliva
+// chitanje na vrednost na promenliva
+// pristapuvanje na vrednost na promenliva
+console.log(greeting, godina, imeIPrezime)
+
+// console.log(godina)
+
+// console.log(imeIPrezime)
+
+// logichki promenlivi - boolean (true ili false)
+const polnoleten = true
+
+// vrednosti na promenlivi od nevaliden tip (faulty values) - undefined, null, NaN, false, 0, ' ', ...
+let test
+console.log(test)
+
+// console.warn se koristi za poraki so izvichnik - warning
+// console.error se koristi za poraki so error
+
+const a = 5
+const b = 6
+b = b + 2 // b = 8 , moze da se zapishe i kako b += 2
+// b++ e zgolemuvanje na b za +1
+// const c = a + b
+// console.log(c)
+// opcii za matematichki operacii console.log(c, a + b, a / b, a * b, a % b)
+// a % b - ostatok od operacija na delenje
+
+console.log(a + b)
+
+let ime = "petko"
+let prezime = "petkovski"
+console.log(ime + ' ' + prezime)
